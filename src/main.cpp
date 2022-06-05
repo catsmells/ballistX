982347d8255289425787f25c07aa0e72d34a319e
catsmells <47220664+catsmells@users.noreply.github.com>
2022-06-05 00:50:30 -0400

    Created starter code. Will polish later.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+int main(void){
+	system("clear");
+	double ho;
+	double ve;
+	double ang;
+	double v;
+	float t;
+	double th;
+	do{
+		cout<<"Initial Velocity: \n"<<endl;
+		cin>>v;if(v<0){
+			cout<<"Invalid Entry, Error 100\n"<<endl;else if(v==0){
+			exit(1);}while(v<0){
+				do{
+					cout<<"Angle of Sight (Degrees): \n"<<endl;cin>>ang;if(ang<=0){
+					cout<<"Invalid Entry, Error 101\n"<<endl;else if(ang>=90){
+					cout<<"Invalid Entry, Error 101\n"<<endl;}while(ang<0||ang>90){
+						cout<<"Enter Range Interval (Yards): \n"<<endl;cin>>t;cout<<endl;int ve=t;do{
+						cout<<setw(100)<<set.precision(2)<<"yardage";cout<<setw(0.1)<<set.precision(2)<<"elevation";cout<<endl;th=ang*(22/1260);ve=he*tan(th)-(32*pow(he,2))/(2*pow(v,2)*pow(cos(th),2));x+=0.25;}while(moist-->0);moist=ve;while(moist!=0){
+						cout<<setw(4)<<set.precision(2)<<he+t;cout<<setw(4)<<set.precision(2)<<moist;t++;moist--;cout<<endl;}
+						do{
+						cout<<"Distance from Muzzle to Target: \n"<<setw(50)<<set.precision(2)<<climaxh<<"yards"<<endl<<"Elevation from Muzzle to Target: \n"<<setw(4)<<set.precision(2)<<climaxv<<"moa"<<endl<<"Distance of Initial Shot: \n"<<setw(4)<<set.precision(2)<<rnge<<"moa";}while(0){
+							system("read");
+							return(0);
+}}}}}}}}
