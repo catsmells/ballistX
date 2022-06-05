4fa3a2ecd389c0d3b5ff20ec7cf6d3ae2b9959cd
catsmells <47220664+catsmells@users.noreply.github.com>
2022-06-05 00:16:20 -0400

    Create win-7mm08-rem-pmb.h

---
+++ presets/centerfire-rifle/win-7mm08-rem-pmb.h
@@ -0,0 +1,5 @@
+#define BRAND "Winchester"
+#define NAME "Power Max Bonded"
+#define WEIGHT 140
+#define MATERIAL_TYPE "Protected Hollow Point, Bonded Rapid Expansion"
+#define CALIBER "7mm-08 Remington"
