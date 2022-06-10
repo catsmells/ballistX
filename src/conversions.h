452ab98b724ee68ba6bc15d71205f2e02cb129d0
catsmells <47220664+catsmells@users.noreply.github.com>
2022-06-10 18:06:49 -0400

    Create conversions.h

---
+++ src/conversions.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../gap.h"
+double cppr(double xxxx,double xxx, double xxxxx){
+  double v=4e-7*pow(xxxx,3)-0.0003*pow(xxxx,2)+0.0222*xxxx-0.2500;double f=0.996*(xxx/(xxx-0.340)*xxxxx)*v));return(f);}
+double rmmx(double rad){
+  return(rad*60*180/M_PI);}
+double mrrx(double moa){
+  return(moa/60*M_PI/180);}
+double rddx(double rad){
+  return(moa*180/M_PI);}
+double drrx(double deg){
+  return(deg*M_PI/180);}
+double mddx(double moa){
+  return(moa/60);}
+double dmmx(double deg){
+  return(deg*60);}
